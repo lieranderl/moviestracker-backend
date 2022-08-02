ccdbb8f6d30de0673ec5f4f5887cb7d20614759d
evfedoto <evfedoto@cisco.com>
2022-08-02 23:00:58 +0200

    .

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	// "github.com/aws/aws-lambda-go/lambda"
 	"github.com/joho/godotenv"
+	// "github.com/aws/aws-lambda-go/lambda"
 	// "github.com/joho/godotenv"
 )
 
--- tapochek/tapochek.go
+++ tapochek/tapochek.go
@@ -27,15 +27,13 @@ func verifyLogin(c *colly.Collector, login, pass string) bool {
 	u, _ := url.Parse("https://tapochek.net")
 	for _, j := range httpClient.Jar.Cookies(u) {
 		if j.Name == "bb_data" {
-			if err != c.SetCookies("https://tapochek.net", httpClient.Jar.Cookies(u)) {
-				return false
-			}
-			// return err == c.SetCookies("https://tapochek.net", httpClient.Jar.Cookies(u))
-			return true
+			log.Println("LOGIN TAPOCHEK")
+			return c.SetCookies("https://tapochek.net", httpClient.Jar.Cookies(u)) == nil
 		} else {
+			log.Println("LOGIN TAPOCHEK WITH COOKIE")
 			coo := &http.Cookie{
 				Name: "bb_data", 
-				Value: "a%3A3%3A%7Bs%3A2%3A%22uk%22%3Bs%3A12%3A%220PLv1Vf4MJhm%22%3Bs%3A3%3A%22uid%22%3Bi%3A19320%3Bs%3A3%3A%22sid%22%3Bs%3A20%3A%221BazNVBL572GaZfFlfoM%22%3B%7D",
+				Value: "a%3A3%3A%7Bs%3A2%3A%22uk%22%3Bs%3A12%3A%22gQo1aJb0OyVK%22%3Bs%3A3%3A%22uid%22%3Bi%3A19320%3Bs%3A3%3A%22sid%22%3Bs%3A20%3A%22q9oFxR63hRTO9O3fBoc7%22%3B%7D",
 				Domain: ".tapochek.net",
 				Path:"/",
 				Expires: time.Now().Add(time.Minute*525600),
